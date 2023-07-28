ea987955a1d1b6f4833921f45ba849a46e5f65c2
iso9000t <sasha.gr81@gmail.com>
2023-07-28 13:09:45 -0300

    feat; add more functions

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -537,8 +537,16 @@ function distinct(/* arr */) {
  *    "Poland" => ["Lodz"]
  *   }
  */
-function group(/* array, keySelector, valueSelector */) {
-  throw new Error('Not implemented');
+function group(array, keySelector, valueSelector) {
+  return array.reduce((accumulator, item) => {
+    const key = keySelector(item);
+    const value = valueSelector(item);
+    if (!accumulator.has(key)) {
+      accumulator.set(key, []);
+    }
+    accumulator.get(key).push(value);
+    return accumulator;
+  }, new Map());
 }
 
 
@@ -555,8 +563,8 @@ function group(/* array, keySelector, valueSelector */) {
  *   [[1, 2], [3, 4], [5, 6]], (x) => x     =>   [ 1, 2, 3, 4, 5, 6 ]
  *   ['one','two','three'], (x) => x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
+function selectMany(arr, childrenSelector) {
+  return arr.flatMap(childrenSelector);
 }
 
 
@@ -595,8 +603,12 @@ function getElementByIndexes(/* arr, indexes */) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  const mid = Math.floor(arr.length / 2);
+  if (arr.length % 2 === 0) {
+    return [...arr.slice(mid), ...arr.slice(0, mid)];
+  }
+  return [...arr.slice(mid + 1), arr[mid], ...arr.slice(0, mid)];
 }
 
 
